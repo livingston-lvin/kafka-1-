2e9e85c7fba18e762f459cbc579234a521d63a30
livingston-lvin <livingstonlvin@gmail.com>
2023-02-13 02:57:48 +0530

    first commit

---
+++ src/test/java/com/lvin/Kafka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
